15e8d708c20fcb020774aad48fcf4a4cd1257e60
leoxing <95263603+leoxing66@users.noreply.github.com>
2021-12-03 10:56:17 +0800

    添加了lombok插件，并测试了一下

--- src/main/java/com/bjsxt/test/TestA.java
+++ src/main/java/com/bjsxt/test/TestA.java
@@ -1,12 +1,10 @@
 package com.bjsxt.test;
 
+import com.bjsxt.entity.Student;
 import com.bjsxt.entity.User;
 import com.bjsxt.mapper.UserMapper;
 import com.bjsxt.util.DBUtil;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.session.SqlSession;
 
-import java.util.List;
 
 public class TestA {
 
@@ -14,5 +12,6 @@ public class TestA {
         UserMapper mapper = DBUtil.getSqlSession().getMapper(UserMapper.class);
         User user = mapper.selectOne("zhangsan","123");
         System.out.println(user);
+        Student s = new Student();
     }
 }
